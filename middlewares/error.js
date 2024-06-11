b93a1c7af6aa0243062ed4ce569a0b1f919af93f
philapulga10 <phinguyenuit10@gmail.com>
2024-06-11 18:41:42 +0700

    cookie options

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,4 +1,4 @@
-export const errorMiddleware = (err, req, res, next) => {
+export const errorMiddleware = (err, _, res) => {
   err.message = err.message || "Internal server error";
   err.statusCode = err.statusCode || 500;
 
--- utils/features.js
+++ utils/features.js
@@ -4,7 +4,14 @@ export const sendToken = (user, res, message, statusCode) => {
   res
     .status(statusCode)
     .cookie("token", token, {
+      ...cookieOptions,
       expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
     })
     .json({ success: true, message });
+};
+
+export const cookieOptions = {
+  secure: process.env.NODE_ENV === "Development" ? false : true,
+  httpOnly: process.env.NODE_ENV === "Development" ? false : true,
+  sameSite: process.env.NODE_ENV === "Development" ? false : true,
 };
