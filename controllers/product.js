a79dfe215d019ebb64a909c8880bea139a85762b
philapulga10 <phinguyenuit10@gmail.com>
2024-06-12 18:35:00 +0700

    update product vs add product image

--- controllers/product.js
+++ controllers/product.js
@@ -57,3 +57,65 @@ export const createProduct = asyncError(async (req, res, next) => {
     message: "Product created successfully",
   });
 });
+
+export const updateProduct = asyncError(async (req, res, next) => {
+  const { name, description, category, price, stock } = req.body;
+
+  const product = await Product.findById({ _id: req.params.id });
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+
+  if (name) {
+    product.name = name;
+  }
+  if (description) {
+    product.description = description;
+  }
+  if (category) {
+    product.category = category;
+  }
+  if (price) {
+    product.price = price;
+  }
+  if (stock) {
+    product.stock = stock;
+  }
+
+  await product.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Product updated successfully",
+  });
+});
+
+export const addProductImage = asyncError(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  if (!req.file) {
+    return next(new ErrorHandler("Please add image", 404));
+  }
+
+  const file = getDataUri(req.file);
+
+  const myCloud = await cloudinary.v2.uploader.upload(file.content);
+
+  const image = {
+    public_id: myCloud.public_id,
+    url: myCloud.secure_url,
+  };
+
+  product.images.push(image);
+
+  await product.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Image added successfully",
+  });
+});
--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,11 @@
 import express from "express";
 
 import {
+  addProductImage,
   createProduct,
   getAllProducts,
   getProductDetails,
+  updateProduct,
 } from "../controllers/product.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 import { singleUpload } from "../middlewares/multer.js";
@@ -12,8 +14,12 @@ const router = express.Router();
 
 router.get("/all", getAllProducts);
 
-router.get("/single/:productId", getProductDetails);
+router.get("/single/:id", getProductDetails);
 
 router.post("/new", isAuthenticated, singleUpload, createProduct);
+
+router.put("/single/:id", isAuthenticated, updateProduct);
+
+router.post("/images/:id", isAuthenticated, singleUpload, addProductImage);
 
 export default router;
