a6b982bd04c97fe5f1646cad1b809aafe55c000a
elpulga <phinguyenuit10@gmail.com>
2024-06-21 14:05:24 +0700

    creating order schema

--- models/order.js
+++ models/order.js
@@ -1,5 +1,91 @@
+import mongoose from "mongoose";
 import moongoose from "mongoose";
 
-const orderSchema = new moongoose.Schema({});
+const orderSchema = new moongoose.Schema({
+  shippingInfo: {
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    pinCode: {
+      type: String,
+      required: true,
+    },
+  },
+  orderItems: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      image: {
+        type: String,
+        required: true,
+      },
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+    }
+  ],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  paymentMethod: {
+    type: String,
+    enum: ["COD", "ONLINE"],
+    default: "COD",
+  },
+  paidAt: Date,
+  paymentInfo: {
+    id: String,
+    status: String,
+  },
+  itemsPrice: {
+    type: Number,
+    required: true,
+  },
+  taxPrice: {
+    type: Number,
+    required: true,
+  },
+  shippingPrice: {
+    type: Number,
+    required: true,
+  },
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  orderStatus: {
+    type: String,
+    enum: ["Preparing", "Shipped", "Delivered"],
+    default: "Preparing"
+  },
+  deliveredAt: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
 
 export const Order = moongoose.model("Order", orderSchema);
